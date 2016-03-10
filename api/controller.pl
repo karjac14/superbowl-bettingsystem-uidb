55dc26fa3187d332591f20563b904211c1988639
Karlo Eleuterio <karloe@gmail.com>
2016-03-10 05:01:32 -0500

    sub json to hash test ok

--- api/controller.pl
+++ api/controller.pl
@@ -4,10 +4,14 @@ use strict;
 use warnings;
 
 use XML::Simple;
+use XML::LibXML;
 use JSON::XS qw(encode_json decode_json);
-use JSON::Parse;
+use JSON::Parse ':all';
 use Dancer;
 use Dancer::Plugin::RequireSSL;
+
+use Test::Simple tests => 2;
+use Data::Dumper;
 
 require_ssl();
 # use Dancer::Response;
@@ -80,13 +84,46 @@ sub getbets {
 
 sub newbet {
   my ($new_bet) = @_;
-  my $newbet_hash = &convertJSONtoHASH($new_bet);
+  my $newbet_ref = &convertJSONtoHASH($new_bet);
+  my %newbet_hash = %{ $newbet };
 
   #generate unique control number
   my @chr = ('0' ..'9', 'A' .. 'Z');
   my $controlno = join ('', map $chr[rand @chr], 1 .. 8);
 
   #TODO: append new bet to XML
+
+  my $parser = XML::LibXML->new();
+  my $bets_details = $parser->parse_file($bets_url) or die;
+
+  my $bet  = $bets_details->findnodes("bets")->[0];
+
+  my $node = XML::LibXML::Element->new("bet");
+
+  my $t = XML::LibXML::Element->new("team");
+  my $tn = XML::LibXML::Text->new($newbet_hash{team});
+  $t->addChild($tn);
+
+  my $a = XML::LibXML::Element->new("amount");
+  my $an = XML::LibXML::Text->new($newbet_hash{amount});
+  $a->addChild($an);
+
+  my $c = XML::LibXML::Element->new("controlno");
+  my $cn = XML::LibXML::Text->new($controlno);
+  $c->addChild($cn);
+
+  $node->addChild($t);
+  $node->addChild($a);
+  $node->addChild($c);
+
+  $bet->addChild($node);
+
+  open(my $fh, '>', $bets_url);
+  print $fh $bet;
+  close $fh;
+
+
+
 }
 
 
@@ -107,12 +144,10 @@ get '/bets' => sub {
 
 post '/newbet' => sub {
     header 'Access-Control-Allow-Origin' => '*';
-    my $newbet_json = decode_json (request->body);
-    my $newbet = &newbet($newbet_json);
-    #convert json to hash
-    #create control number and process
-    #add (push) json to db
-
+    my $newbet_json = request->body;
+    # my $newbet = &
+    newbet($newbet_json);
+    return $newbet_json;
     # &print($p);
 };
 
@@ -121,4 +156,25 @@ options '/newbet' => sub {
     header 'Access-Control-Allow-Origin' => '*';
     header 'Access-Control-Allow-Headers' => 'Content-Type' => 'text/plain';
 };
-dance;
+
+sub tests   {
+    ok( 1 + 1 == 2 );
+
+    my $newbet_json = ('{"betAmount":200,"team":1}');
+    my $newbet = convertJSONtoHASH($newbet_json);
+    my %newbethash = %{ $newbet };
+    my @keys = keys %newbethash;
+    my $size = @keys;
+    ok(scalar $size == 2);
+
+    exit;
+}
+
+print Dumper @ARGV;
+my $myargs = @ARGV;
+if ($myargs >= 1){
+  tests;
+}
+else {
+  dance;
+}
